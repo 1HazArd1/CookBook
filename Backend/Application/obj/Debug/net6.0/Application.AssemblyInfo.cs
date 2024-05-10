e6881ae0a49fd77cfe95e6b9112d5dbebaf9fd78
1HazArd1 <aakashgoingto99@gmail.com>
2024-05-10 19:58:47 +0530

    renamed to fix gitignore

--- Backend/Application/obj/Debug/net6.0/Application.AssemblyInfo.cs
+++ Backend/Application/obj/Debug/net6.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CookBook.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4123feb98e695a3869f569cd28d1193495d40816")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2bdb69e58fd5f33c51b41c226e82c2282079b26")]
 [assembly: System.Reflection.AssemblyProductAttribute("CookBook.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CookBook.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
