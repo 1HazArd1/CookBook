5ba0d962c6e41f144e99537cef34ccfab6fc0db0
1HazArd1 <aakashgoingto99@gmail.com>
2024-05-19 19:31:44 +0530

    login and register fragments and their navigation

--- Frontend/app/src/main/java/com/example/cookbook/UserActivity.kt
+++ Frontend/app/src/main/java/com/example/cookbook/UserActivity.kt
@@ -5,16 +5,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.cookbook.user.LoginFragment
 
-class MainActivity : AppCompatActivity() {
+class UserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_user)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        addFragment()
+    }
+    private fun addFragment(){
+        val userSignInLayout = LoginFragment()
+        supportFragmentManager.beginTransaction().add(R.id.fl_fragment_container, userSignInLayout)
+            .commit()
     }
 }
